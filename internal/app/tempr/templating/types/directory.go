ffa6784c5737ee3dd8afd382454ed7d37be12224
Lolmerkat <git@lolmerkat.cc>
2025-04-29 01:49:52 +0200

    Add: Expanding directories and files

--- internal/app/tempr/templating/types/directory.go
+++ internal/app/tempr/templating/types/directory.go
@@ -1,6 +1,26 @@
 package templating
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/log"
+)
+
 type Directory struct {
-	Name		string		`yaml:"string"`
-	Children	[]FSElement	`yaml:"children"`
+	Name     string      `yaml:"string"`
+	Children []FSElement `yaml:"children"`
+}
+
+func (d *Directory) Expand(path string, logger *log.Logger) {
+	dirPath := fmt.Sprintf("%s/%s/", path, d.Name)
+	err := os.Mkdir(dirPath, os.ModePerm)
+	if err != nil && !os.IsExist(err){
+		logger.Fatalf("Error creating dir '%s': %v", dirPath, err)
+	}
+
+	for _, child := range d.Children {
+		child.Expand(path, logger)
+	}
+	logger.Infof("Created %s", dirPath)
 }
--- internal/app/tempr/templating/types/file.go
+++ internal/app/tempr/templating/types/file.go
@@ -1,6 +1,27 @@
 package templating
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/log"
+)
+
 type File struct {
 	Name		string		`yaml:"name"`
 	Content		[]string	`yaml:"content"`
+}
+
+func (f *File) Expand(path string, logger *log.Logger) {
+	filePath := fmt.Sprintf("%s/%s", path, f.Name)
+	file, err := os.Create(filePath)
+	if err != nil {
+		logger.Fatalf("Error creating file '%s': %v", filePath, err)
+	}
+	defer file.Close()
+
+	for _, line := range f.Content {
+		file.WriteString(line + "\n")
+	}
+	logger.Infof("Created %s", filePath)
 }
