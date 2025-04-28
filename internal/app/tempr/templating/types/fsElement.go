a1e3e294a987c535be6a0b6cc149582dc1b9a6f4
Lolmerkat <git@lolmerkat.cc>
2025-04-29 01:57:27 +0200

    Add: template loading

--- internal/app/tempr/templating/types/fsElement.go
+++ internal/app/tempr/templating/types/fsElement.go
@@ -5,7 +5,6 @@ import (
 )
 
 type FSElement interface {
-	Expand(path string)
 	Expand(path string, logger *log.Logger)
 }
 
@@ -13,4 +12,24 @@ type RawFSElement struct {
 	Name		string			`yaml:"name"`
 	Content		[]string		`yaml:"content"`
 	Children	[]RawFSElement	`yaml:"children,omitempty"`
+}
+
+func (raw RawFSElement) ToFSElement() FSElement {
+	if raw.Content != nil {
+		return &File {
+			Name: raw.Name,
+			Content: raw.Content,
+		}
+	}
+
+	dir := &Directory {
+		Name: raw.Name,
+		Children: make([]FSElement, 0, len(raw.Children)),
+	}
+
+	for _, child := range raw.Children {
+		dir.Children = append(dir.Children, child.ToFSElement())
+	}
+
+	return dir
 }
