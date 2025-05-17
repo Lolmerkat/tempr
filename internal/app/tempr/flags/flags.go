653657e1d0481d208866d04e4f13ea24ba2077d3
Lolmerkat <git@lolmerkat.cc>
2025-05-17 05:24:03 +0200

    Revert: custom flag help

--- internal/app/tempr/flags/flags.go
+++ internal/app/tempr/flags/flags.go
@@ -1,11 +1,6 @@
 package flags
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/charmbracelet/lipgloss"
-	"github.com/lolmerkat/tempr/internal/app/tempr"
 	flag "github.com/spf13/pflag"
 )
 
@@ -25,27 +20,7 @@ func Declare() {
 }
 
 func Handle() {
-	flag.Usage = usage
 	flag.Parse()
 	//vvv  INFO: Flag handlers that should be executed immediately vvv
 	handleLogLevel(logLevelPtr)
-}
-
-func usage() {
-	out := tempr.Logger()
-	usageHeaderStyle := lipgloss.NewStyle().Bold(true)
-	fmt.Fprintf(os.Stderr, "\033[1;34mUsage:\033[0m\n") // Blue bold header
-
-	out.Printf(usageHeaderStyle.Render("Usage:"))
-	fmt.Fprintf(os.Stderr, "  %s [flags]\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "\033[1;33mFlags:\033[0m\n") // Yellow bold "Flags:"
-
-	flag.VisitAll(func(f *flag.Flag) {
-		// Example: green flag name, cyan shorthand, magenta default
-		fmt.Fprintf(os.Stderr, "  \033[32m--%s\033[0m", f.Name)
-		if f.Shorthand != "" {
-			fmt.Fprintf(os.Stderr, ", \033[36m-%s\033[0m", f.Shorthand)
-		}
-		fmt.Fprintf(os.Stderr, " \033[35m(default: %s)\033[0m\n      %s\n", f.DefValue, f.Usage)
-	})
 }
